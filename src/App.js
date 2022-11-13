4d0b011a2ff5488f0572e1dad5f2df815e7c25d6
dina-25 <>
2022-11-13 08:02:42 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './styles/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    const [orders, setOrders] = useState([]);
+
+    //React hppk to render data once they are available
+    useEffect(() => {
+        //async function to fetch data from cart 
+        const fetchCartData = async () => {
+            const response = await fetch('https://fakestoreapi.com/carts');
+            const cartData = await response.json();
+
+            setOrders(cartData)
+        }
+        fetchCartData();
+    }, []);
+
+    return ( 
+    <div className = "App" >
+        <header className = "Cart Details">
+        <h2>Orders Details</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>User#</th>
+                    <th>Order date</th>
+                    <th>product</th>
+                    <th>quantity</th>
+                    <th>Invoice</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    orders.map((cart,i) =>
+                    <tr key={i}>
+                        <td>{cart.userId}</td>
+                        <td>{cart.date}</td>
+                        <td>{cart.products.map((item, index) =>
+                            <React.Fragment key={index}>
+                                {item.productId}
+                                <br/>
+                            </React.Fragment>
+                        )}</td>
+                        <td>{cart.products.map((item, index) =>
+                            <React.Fragment key={index}>
+                                {item.quantity} 
+                                <br/>
+                            </React.Fragment>
+                            
+                        )}</td>
+                        <td>
+                            <button className='invoiceButton' onClick={()=> alert("Function will be available soon")}>Send Invoice</button>
+                        </td>
+                    </tr>
+                     )}
+                
+            </tbody>
+        </table>
+        </header>
     </div>
-  );
+    );
+    
 }
+
+
 
 export default App;
